17ee1447d5c9044fe241c2360f8a535c3dfd4b7d
Valentina Pinzón Montoya <valenpinzonn@gmail.com>
2023-12-11 23:32:06 -0500

    18 subpages with code

---
+++ src/pages/subpages/promoter/CulturalHumility.jsx
@@ -0,0 +1,90 @@
+
+import React from 'react';
+import '../../../components/shared/buttons/Button'; 
+import '../../../components/shared/buttons/ButtonSkills'; 
+import {  } from "../../../styles/Profile.css";
+import {} from '../../../styles/Header.css'
+import {} from '../../../styles/Titles.css'
+import {  } from "../../../pages/home";
+import Menu from '../../../components/menu2';
+import SearchBar2 from "../../../components/searchbar2";
+import TopicsCard from '../../../components/shared/cards/TopicsCard';
+import Footer from '../../../components/footer';
+
+
+const CulturalHumility = () => {
+  return (
+    <div style={{ maxWidth:'1920px',  backgroundColor:'white' }}> 
+      <main  id="main1" className="main1" >
+      <Menu></Menu>
+      <div style={{position:"relative", backgroundColor:"transparent", left:'100px', width: '1440px', boxSizing: 'border-box',top:"50px" }}>
+      <div style={{position:'relative',top:"-90px", boxSizing: 'border-box', zIndex: 1 }}>
+        <SearchBar2 />
+      </div>
+      <div className='row'>
+        <div className='col-12'>
+          <p className='title-route'>Home - CHWs / Promotores - Personal and Professional Development - Cultural Humility</p>
+            <div className='col-6'>
+              <h1 className='title-h1-subpages' style={{ paddingTop: '35px'}}>Cultural Humility</h1>
+              <p className='subtitle-subpages'>Refine by type of resource</p>
+            </div>
+
+
+{/* ---------------------------------------PONER CARD OVERVIEW */}
+
+        <div>
+          <h1 className='title-resources' style={{ paddingTop: '35px', position: 'relative', top: 0, fontWeight: 'bold'}}>Resources</h1>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'}  title="Personal reflection activity
+printable" subtitle="Cultural Humility" text="Community Health Workers (CHWs) are trusted members of the communities where they live..."/>
+          </div>
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'} className="mx-4" title="A221D - Personal Reflection
+Activity Digital"  subtitle="Cultural Humility" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus neque, fermentum nec..."/>
+          </div>
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'} title="241-VTP-The wheel of diversity: 
+Personal Reflexión for Cultural
+Humility " subtitle="Cultural Humility" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus neque, fermentum nec..."/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'}  title="241VT - Cultural Humility
+Iceberg Printable" subtitle="Cultural Humility" text="Community Health Workers (CHWs) are trusted members of the communities where they live..."/>
+          </div>
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'} className="mx-4" title="242 - Tips for developing
+cultural humility"  subtitle="Cultural Humility" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus neque, fermentum nec..."/>
+          </div>
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'} title="2113 - The 7 C’s of Cultural Humility" subtitle="Cultural Humility" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus neque, fermentum nec..."/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'}  title="243 - Cultural Humility Activity
+Sheet" subtitle="Cultural Humility" text="Community Health Workers (CHWs) are trusted members of the communities where they live..."/>
+          </div>
+          <div className="col-sm-4">
+            <TopicsCard  img={'/mg-2/AgenciesCard.png'} className="mx-4" title="What is Cultural Humility?
+Video"  subtitle="Cultural Humility" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus neque, fermentum nec..."/>
+          </div>
+        </div>
+      </div>
+    </div>
+          
+      </div>
+          </main>
+          <div style={{position:'relative', top:'450px', backgroundColor:'#003d6a'}}>
+              <Footer />
+          </div>
+    </div>
+
+  );
+};
+
+export default CulturalHumility;
